d42993a8a97b395eb0c4295d167d893cecab8c48
rezafset17 <rezafset17@gmail.com>
2020-11-17 03:18:51 +0600

    Connect to Mongo & create two API

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
+const PORT = process.env.PORT || 7000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static('service'));
+app.use(fileUpload());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y5kfv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const houseCollection = client.db("apartmentHunt").collection("house");
+
+    app.post('/addHouse', (req, res) => {
+        const file = req.files.file;
+        const title = req.body.title;
+        const price = req.body.price;
+        const location = req.body.location;
+        const bedroom = req.body.bedroom;
+        const bathroom = req.body.bathroom;
+
+        const newImg = file.data;
+        const encImg = newImg.toString('base64');
+
+        // console.log(file, title, price, location, bedroom, bathroom);
+        var image = {
+            contentType: file.mimetype,
+            size: file.size,
+            img: Buffer.from(encImg, 'base64')
+        };
+
+        houseCollection.insertOne({ title, price, location, bedroom, bathroom, image })
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+
+    });
+
+    // House List
+    app.get('/house', (req, res) => {
+        houseCollection.find({})
+            .toArray((error, document) => {
+                res.send(document);
+            })
+    })
+
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Apertment Hunt Server');
+})
+
+app.listen(PORT)
