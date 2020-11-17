93c618ae5309620ddfed3b435bfbea40ce563e56
rezafset17 <rezafset17@gmail.com>
2020-11-17 21:29:30 +0600

    Database Done

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
     const houseCollection = client.db("apartmentHunt").collection("house");
     const rentCollection = client.db("apartmentHunt").collection("rent");
+    const adminCollection = client.db("apartmentHunt").collection("admin");
 
     // Add house
     app.post('/addHouse', (req, res) => {
@@ -43,7 +44,6 @@ client.connect(err => {
             .then(result => {
                 res.send(result.insertedCount > 0)
             })
-
     });
 
     // House List
@@ -56,22 +56,75 @@ client.connect(err => {
 
     // Get selected apt data
     app.get('/apartmentDetails/:id', (req, res) => {
-    houseCollection.find({
-      _id: ObjectId(req.params.id)
-    })
-    .toArray((err, documents) => {
-        res.send(documents[0])
+        houseCollection.find({
+            _id: ObjectId(req.params.id)
         })
+            .toArray((err, documents) => {
+                res.send(documents[0])
+            })
     })
-    
+
     // Request Apartment Booking
     app.post('/requestBooking', (req, res) => {
-        const receivedData = req.body;
-        console.log(receivedData);
+        // const receivedData = req.body;
+        const title = req.body.title;
+        const location = req.body.location;
+        const price = req.body.price;
+        const name = req.body.name;
+        const email = req.body.email;
+        const phone = req.body.phone;
+        const message = req.body.message;
+        const status = req.body.status;
+
+        rentCollection.insertOne({ name, email, phone, message, status, title, location, price })
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    // All Rent List
+    app.get('/rent', (req, res) => {
+        rentCollection.find({})
+            .toArray((error, document) => {
+                res.send(document);
+            })
+    });
+
+    app.post('/userRent', (req, res) => {
+        const email = req.body.email;
+        rentCollection.find({ email: email })
+            .toArray((error, user) => {
+                res.send(user)
+            })
+    })
+
+    // Add Admin
+    app.post('/addAdmin', (req, res) => {
+        const admin = req.body;
+        // console.log(review);
+        adminCollection.insertOne(admin)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    // Admin List
+    app.get('/admin', (req, res) => {
+        adminCollection.find({})
+            .toArray((error, document) => {
+                res.send(document);
+            })
+    })
+    // Check Admin
+    app.post('/isAdmin', (req, res) => {
+        const email = req.body.email;
+        adminCollection.find({ email: email })
+            .toArray((error, admin) => {
+                res.send(admin.length > 0)
+            })
     })
 
 });
-
 
 app.get('/', (req, res) => {
     res.send('Welcome to Apertment Hunt Server');
